f23f6139f2d58992c1f09b97abadcee21ee85f15
JHSeng <zxc98567@163.com>
2019-12-07 22:34:10 +0800

    add mmbot

---
+++ turtle_game_design/src/turtle_game/build/CMakeFiles/subscriber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/subscriber.dir/subscriber.cpp.o"
+  "/home/jhseng/catkin_ws/src/turtle_game/devel/lib/turtle_game/subscriber.pdb"
+  "/home/jhseng/catkin_ws/src/turtle_game/devel/lib/turtle_game/subscriber"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/subscriber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
